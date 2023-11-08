0d26561f14393c2010a73edf3a81c1c3a993796b
Elia Kuzmanova <eliamitova7@gmail.com>
2023-11-08 14:09:40 +0100

    add global css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import {Footer, Blog, Header, Features, Possibility, WhatGPT3} from './containers'
 import {Cta, Brand, Navbar} from './components'
+import './App.css'
 
 const App = () => {
   return (
